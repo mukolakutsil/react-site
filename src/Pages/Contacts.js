c5ec0a82be3ccfbdfa14fd0d0c0ffd5a50a91e15
Mykola_Kutsil <mukola.kutsil22@gmail.com>
2021-03-14 21:42:27 +0200

    creating_Form

--- src/Pages/Contacts.js
+++ src/Pages/Contacts.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
+import { Container, Form, Button } from 'react-bootstrap';
 
 class Contacts extends Component {
     render() {
         return (
-            <div>
-               <h1>Contacts</h1> 
-            </div>
+            <Container style={{ width: "500px" }}>
+
+                <h1 classNama="text-center">Contact us</h1>
+
+                <Form>
+                    <Form.Group controlId="formBasicEmail">
+                        <Form.Label>Email adress</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" />
+                        <Form.Text>
+                            We'll newer share your email with anyone else
+                        </Form.Text>
+                    </Form.Group>
+
+                    <Form.Group controlId="formBasicPassword">
+                        <Form.Label>Example textarea</Form.Label>
+                        <Form.Control as="textarea" rows="3" />
+                    </Form.Group>
+
+                    <Form.Group controlId="formBasicCheckbox">
+                        <Form.Check type="checkbox" label="Check me out" />
+                    </Form.Group>
+                    <Button variant="primary" type="submit">Submit</Button>
+                </Form>
+
+            </Container>
         );
     }
 }
